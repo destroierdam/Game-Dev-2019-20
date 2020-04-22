84e729db4ed5ddfd3a05b8e66a958d43df5cc724
destroierdam <dam-bb@hotmail.com>
2020-04-22 13:13:52 +0300

    Add Homework 5

---
+++ Homeworks/HW5-Healthbar/Assets/AIHealthBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AIHealthBar : MonoBehaviour
+{
+    private GameObject AI;
+    private Animator AIAnimator;
+    private Vector3 offset;
+    private Slider slider;
+    void Start() {
+        AI = GameObject.FindGameObjectWithTag("AI");
+        if (AI == null)
+        {
+            Debug.LogError("GameObject AI not found");
+            return;
+        }
+        AIAnimator = AI.GetComponent<Animator>();
+        slider = GetComponent<Slider>();
+
+        offset = this.transform.position - AI.transform.position;
+    }
+
+    void Update() {
+        int AIHealth = AIAnimator.GetInteger("Health");
+        slider.value = AIHealth;
+
+        this.transform.position = AI.transform.position + offset;
+    }
+}
---
+++ Homeworks/HW5-Healthbar/Assets/Scripts/PlayerHealthBar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthBar : MonoBehaviour
+{
+    GameObject player;
+    /// <summary>
+    ///  The offset of the position of the player and the HealthBar
+    /// </summary>
+    private Vector3 offset;
+    private Animator playerAnimator;
+    private Slider slider;
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (player == null) {
+            Debug.LogError("GameObject Player not found");
+            return;
+        }
+        playerAnimator = player.GetComponent<Animator>();
+        slider = GetComponent<Slider>();
+
+        offset = this.transform.position - player.transform.position;
+    }
+
+    void Update() {
+        int playerHealth = playerAnimator.GetInteger("Health");
+        slider.value = playerHealth;
+
+        this.transform.position = player.transform.position + offset;
+    }
+}

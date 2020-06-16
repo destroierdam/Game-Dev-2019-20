23cab9c5f605c19035088ba45f6fe1a5934f55f5
destroierdam <dam-bb@hotmail.com>
2020-06-16 14:55:35 +0300

    Add attack animations

---
+++ CourseProject/New Unity Project/Assets/Attacks.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Controls;
+
+public class Attacks : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.animator = GetComponent<Animator>();
+        this.animator.SetInteger("AttackType", 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isAttacking = Input.GetKeyDown(Controls.attackKey);
+        bool freezeAttack = Input.GetKeyDown(Controls.freezeAttackKey);
+        bool breakAttack = Input.GetKeyDown(Controls.breakAttackKey);
+        if (freezeAttack) {
+            this.animator.SetInteger("AttackType", 1);
+        } else if (breakAttack) {
+            this.animator.SetInteger("AttackType", 2);
+        }
+        this.animator.SetBool("Attacking", isAttacking);
+    }
+}

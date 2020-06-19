2d29d3667602f7a391312a9aba6349b17623ef30
destroierdam <dam-bb@hotmail.com>
2020-06-19 14:14:55 +0300

    Add mushrooms and animations

---
+++ CourseProject/New Unity Project/Assets/Scripts/Enemy/EnemyMovementController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.Mathf;
+
+public class EnemyMovementController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 2.58f;
+
+    [SerializeField]
+    private float distance = 1.46f;
+
+    private float coveredDistance = 0;
+    private Vector2 velocity = new Vector2(1, 0);
+    private new Rigidbody2D rigidbody;
+    void Start()
+    {
+        this.rigidbody = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        Debug.Log(coveredDistance);
+        Move();
+        if (coveredDistance >= distance)
+        {
+            SwapDirection();
+            coveredDistance = 0;
+        }
+    }
+    private void Move()
+    {
+        Vector2 newPosition = new Vector2
+        {
+            x = velocity.x * speed,
+            y = velocity.y
+        } * Time.fixedDeltaTime + rigidbody.position;
+        rigidbody.MovePosition(newPosition);
+        coveredDistance += Abs(velocity.x * speed * Time.fixedDeltaTime);
+    }
+    private void SwapDirection()
+    {
+        transform.localScale = new Vector3(-transform.localScale.x, 1, 1);
+        velocity.x = -velocity.x;
+    }
+}
